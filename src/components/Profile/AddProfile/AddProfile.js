c69c43cbb69952e0aeeb6a516fd14cb076acc1ef
samirdewan1995 <samirdewan1995@gmail.com>
2022-04-12 11:38:41 +0100

    add profile progress

--- src/components/Profile/AddProfile/AddProfile.js
+++ src/components/Profile/AddProfile/AddProfile.js
@@ -6,32 +6,42 @@ import "./AddProfile.scss";
 
 export default function AddProfile() {
   const [link, setLink] = useState("");
-
+  const interestLister = (e) => {
+    let array = [];
+    for (let i in e.interests) {
+      if (e.interests[i].checked) {
+        array.push(e.interests[i].id)
+      }
+    }
+    console.log(array)
+    return array;
+  }
   const postData = async (event) => {
     event.preventDefault();
     const newPerson = {
       firstName: event.target.firstName.value,
       lastName: event.target.lastName.value,
-      photo: event.target.photo.value,
       nationality: event.target.nationality.value,
       townOfOrigin: event.target.townOfOrigin.value,
       addressOfOrigin: event.target.addressOfOrigin.value,
+      photo: event.target.photo.value,
       email: event.target.email.value,
-      interests: event.target.interests.value,
+      interests: interestLister(event.target),
       birthday: event.target.birthday.value,
       description: event.target.description.value,
       address: event.target.address.value,
-      createdAt: new Date()
-    }
+      postcode: event.target.postcode.value,
+      createdAt: new Date(),
+    };
     const post = await axios.post(
       GET_CONNECT_API_URL,
       newPerson
     );
     setLink(`/home`);
   };
-  console.log("return: ", link);
+
   return (
-    <div className="item-details">
+    <div className="add-profile__wrapper">
       <h1>Enter Profile</h1>
       <form onSubmit={postData} className="add-profile__form">
         <label>
@@ -44,7 +54,7 @@ export default function AddProfile() {
         </label>
         <label>
           Nationality
-          <select name="Choose a Nationality" id="chooseNationality">
+          <select name="nationality" id="chooseNationality">
             <option value="blank"> </option>
             <option value="Bangladesh">Bangladesh</option>
             <option value="Portugal">Portugal</option>
@@ -54,7 +64,7 @@ export default function AddProfile() {
         <label>
           Town of Origin
           <input
-            name="Town of Origin"
+            name="townOfOrigin"
             className="add-profile__field"
             type="text"
           />
@@ -62,40 +72,59 @@ export default function AddProfile() {
         <label>
           Address of Origin
           <input
-            name="Address of Origin"
+            name="addressOfOrigin"
             className="add-profile__field"
             type="text"
           />
         </label>
         <label>
           E-mail
-          <input name="E-mail" className="add-profile__field" type="text" />
+          <input name="email" className="add-profile__field" type="text" />
         </label>
-        <div>
-                        <input type="checkbox" id="archery" name="archery"></input>
-        </div>
-        {/* <options value="bowling">Bowling</options>
-            <options value="pub">Pub</options>
-            <options value="football">Football</options>
-            <options value="hiking">Hiking</options>
-            <options value="cricket">Cricket</options>
-            <options value="music">Music</options>
-            <options value="film">Film</options> */}
+        <fieldset>
+          <legend>Interests</legend>
+          <input type="checkbox" name="interests" value="music" id="music" />
+          <label for="music">Music</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="books" id="books" />
+          <label for="books">Books</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="sport" id="sport" />
+          <label for="sport">Sport</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="pub" id="pub" />
+          <label for="pub">Pub</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="travelling" id="travelling" />
+          <label for="travelling">Travelling</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="film" id="film" />
+          <label for="film">Film</label>
+          <br></br>
+          <input type="checkbox" name="interests" value="games" id="games" />
+          <label for="games">Games</label>
+          <br></br>
+        </fieldset>
+
         <label>
           Birthday
-          <input name="Birthday" className="add-profile__field" type="text" />
+          <input name="birthday" className="add-profile__field" type="text" />
         </label>
         <label>
           Description
           <input
-            name="Description"
+            name="description"
             className="add-profile__field"
             type="text"
           />
         </label>
         <label>
           Address
-          <input name="Address" className="add-profile__field" type="text" />
+          <input name="address" className="add-profile__field" type="text" />
+        </label>
+        <label>
+          Postcode
+          <input name="postcode" className="add-profile__field" type="text" />
         </label>
         <button type="submit">CONFIRM</button>
       </form>
